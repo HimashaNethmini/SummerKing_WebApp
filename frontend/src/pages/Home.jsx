3a28e0bafa4a4c7603fb6b5477ae10b67cec5d8d
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-25 22:16:29 +0530

    Create the homepage

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Announce from "../components/Announce.jsx";
+import Navbar from '../components/Navbar.jsx';
+
+function Home (){
+  return (
+    <div >
+      <Announce/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home;
+
