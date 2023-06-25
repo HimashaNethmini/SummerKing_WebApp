93a9845064c4de79020e4ba9795ea581971051ab
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-25 22:26:26 +0530

    Setup the slider page

---
+++ frontend/src/components/Slider.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Slider() {
+  return (
+    <div className="parentDiv">
+        {/*leftarrow div*/}
+        <div className="leftarrow">
+
+        </div>
+
+        {/*slide div*/}
+        <div className="slide"></div>
+
+        {/*right arrow */}
+        <div className="rightarrow"></div>
+      
+    </div>
+  )
+}
+
+export default Slider;
