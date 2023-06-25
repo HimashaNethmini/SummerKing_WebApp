d80b0efacc293741b3cf2e957206e4fd9eb9cd83
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-25 22:16:47 +0530

    App updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Home from "./page/Home";
 
-function App() {
-  return (
+const App = () => {
+  return ( 
     <div>
+      <Home/>
       
     </div>
   )
 }
 
+export default App;
