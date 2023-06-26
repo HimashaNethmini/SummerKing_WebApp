a04cb433c0f3f7c49718763df955eb83749948ed
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-26 22:30:51 +0530

    Add category

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Announce from "../components/Announce.jsx";
 import Navbar from '../components/Navbar.jsx';
 import Slider from '../components/Slider.jsx';
+import Categories from '../components/Categories.jsx';
 
 function Home (){
   return (
@@ -9,6 +10,7 @@ function Home (){
       <Announce/>
       <Navbar/>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
