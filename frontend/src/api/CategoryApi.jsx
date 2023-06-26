f93beb1013afb944b1fab88ec8a8826bd547529f
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-26 22:56:35 +0530

    Add products - homepage

--- frontend/src/api/CategoryApi.jsx
+++ frontend/src/api/CategoryApi.jsx
@@ -8,7 +8,7 @@ export const ApiCategories =[
         src : 'https://images.pexels.com/photos/6770028/pexels-photo-6770028.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     },
     {
-        title: 'Style Tee Shirts',
+        title: 'Tee Shirts',
         src : 'https://images.pexels.com/photos/8346230/pexels-photo-8346230.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260'
     }
 ]
--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -4,7 +4,8 @@ const Category = ({item}) => {
 
   return (<div className='flex-1 m-2 shadow-lg rounded-md overflow-hidden relative'>
       <img src={item.src} className="w-[100%]" alt='category_img'/>
-      <div className='flex absolute w-[100%] h-[100%] left-0 top-0 items-center justify-center flex-col'>
+      <div className='flex absolute w-[100%] h-[100%] left-0 top-0 
+      items-center justify-center flex-col'>
           <h2 className='text-white font-medium text-[30px]'>{item.title}</h2>
           <buttton className='btn'>See more</buttton>
       </div>
