85e128d170df843fbf072498ae17e650d9860e6f
HimashaNethmini <himashanethmini05@gmail.com>
2023-06-28 10:04:40 +0530

    Updated login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -23,6 +23,39 @@ export default class Login extends Component {
             <div className='flex justify-center w-[100%] h-[100vh] bg-[#b892f7]'>
                 <div className="flex flex-col absolute top-[30%] bg-white shadow-lg border-silver border-[2px] rounded-lg p-5 w-[40%] mobile:w-[90%]">
                     <text className='text-2xl '> Log in </text>
+
+                    {/* first name last name */}
+                    <div className='flex mt-7'>
+                        {/*Email */}
+                        <input 
+                            className={this.inputStyle + ` mt-2 w-[100%]`}
+                            name = "email"
+                            type="email"
+                            placeholder='Write email address'
+                            onChange={this.handleChange}
+                            value={this.state.email}
+                            required 
+                        />
+                    </div>
+
+                    {/* password */}
+                    <div className='flex mt-7'>
+                        <input 
+                            className={this.inputStyle + ` w-[100%]`}
+                            name = "password"
+                            type="password"
+                            placeholder='Enter password'
+                            onChange={this.handleChange}
+                            value={this.state.password}
+                            required 
+                        />
+                    </div>
+                    {/* submit button */}
+                    <input 
+                        type="button" 
+                        className="mt-5 flex justify-center bg-[#8a4af3] text-white font-medium rounded-md p-2 hover:bg-white hover:text-[#8a4af3] hover:scale-[1.0.5] hover:border-[2px] hover:shadow-md hover:border-[#8a4af3] ease-linear duration-200"
+                        value="Log in"
+                        disabled />
                 
                 
                 </div>   
